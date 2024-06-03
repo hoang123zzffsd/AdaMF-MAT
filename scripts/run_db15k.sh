cf571ff1bba1aac85762c61fcc13db56f0c47f3d
hoang123zzffsd <88784062+hoang123zzffsd@users.noreply.github.com>
2024-06-03 15:40:11 +0700

    Update run_db15k.sh

--- scripts/run_db15k.sh
+++ scripts/run_db15k.sh
@@ -6,12 +6,12 @@ LR=2e-5
 NEG_NUM=128
 EPOCH=1000
 
-CUDA_VISIBLE_DEVICES=0 nohup python run_adamf_mat.py -dataset=$DATA \
+CUDA_VISIBLE_DEVICES=0 nohup python /content/AdaMF-MAT/run_adamf_mat.py -dataset=$DATA \
   -batch_size=$NUM_BATCH \
   -margin=$MARGIN \
   -epoch=$EPOCH \
   -dim=$EMB_DIM \
   -mu=0 \
-  -save=./checkpoint/$DATA-$NUM_BATCH-$EMB_DIM-$NEG_NUM-$MARGIN-$LR-$EPOCH \
+  -save=/content/AdaMF-MAT/checkpoint/$DATA-$NUM_BATCH-$EMB_DIM-$NEG_NUM-$MARGIN-$LR-$EPOCH \
   -neg_num=$NEG_NUM \
   -learning_rate=$LR > $DATA-$EMB_DIM-$NUM_BATCH-$NEG_NUM-$MARGIN-$EPOCH.txt &
